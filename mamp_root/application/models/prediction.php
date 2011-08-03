3396ee9109ae8eb02a42523f58102f482170e5fd
Christopher Hunt <iamchampuk@googlemail.com>
2011-08-03 11:12:33 +0100

    Created root directory for mamp, started data models

---
+++ mamp_root/application/models/prediction.php
@@ -0,0 +1,22 @@
+class prediction extends CI_Model {
+
+	var $name   = '';
+	var $source_url = '';
+	var $source_type    = '';
+	var $prediction_value   = '';
+	var $prediction_updated   = '';
+	
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function getPredicition();
+    {
+    
+    
+    } 
+    
+    
+}
---
+++ mamp_root/application/models/realtime.php
@@ -0,0 +1,22 @@
+class realtime extends CI_Model {
+
+	var $name   = '';
+	var $source_url = '';
+	var $source_type    = '';
+	var $realtime_value   = '';
+	var $realtime_updated   = '';
+	
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function getRealTime();
+    {
+    
+    
+    } 
+    
+    
+}
